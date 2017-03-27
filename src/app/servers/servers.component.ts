ba953babebff316e63907c9d4ddec400b00adc51
Zoran Markovic <zoran.markovic@netcentric.biz>
2017-03-27 23:34:14 +0200

    work with components - aditional selectors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-servers',
+    // selector: '[app-servers]',
+    // selector: '.app-servers',
     templateUrl: './servers.component.html',
 })
 export class ServersComponent {
