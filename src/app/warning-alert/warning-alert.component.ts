2743286c7597651d34204b5a18040077f721bdb2
Zoran Markovic <zoran.markovic@netcentric.biz>
2017-03-28 22:47:12 +0200

    assignment - components

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,16 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'app-warning-alert',
+    templateUrl: './warning-alert.component.html',
+    styles: [`
+        p { 
+            padding: 20px;
+            border: 1px solid red;
+            background: mistyrose;
+        }
+    `]
+})
+export class WarningAlertComponent {
+    warning = 'This is the last warning, you are in Danger';
+}
