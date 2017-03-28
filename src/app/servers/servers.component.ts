32383c24b9e86efabcd4a2aac75660d14649eb9b
Zoran Markovic <zoran.markovic@netcentric.biz>
2017-03-28 23:34:52 +0200

    event databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,10 +8,15 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
     allowNewServer: boolean = false;
+    serverCreationStatus: string = 'No server was created!';
 
     constructor() {
         setTimeout(() => {
             this.allowNewServer = true;
         }, 2000)
+    }
+
+    onCreateServer() {
+        this.serverCreationStatus = 'Server is created!';
     }
 }
