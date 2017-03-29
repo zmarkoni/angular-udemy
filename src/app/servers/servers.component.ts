4f849d669ec99b53e3846be4211eb7e633ed0d7d
Zoran Markovic <zoran.markovic@netcentric.biz>
2017-03-29 23:11:48 +0200

    two-way databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 export class ServersComponent {
     disableServer: boolean = true;
     serverCreationStatus: string = 'No server was created!';
-    serverName: any = '';
+    serverName: any = 'test server name';
 
     constructor() {
         setTimeout(() => {
@@ -24,4 +24,12 @@ export class ServersComponent {
     onUpdateServerName(event: Event) {
         this.serverName = (<HTMLInputElement>event.target).value //need like this because of typeScript, regulary it will be event.target.value
     }
+
+    /*
+    Important: For Two-Way-Binding to work, you need to enable the ngModel  directive. This is done by adding the FormsModule  to the imports[]  array in the AppModule.
+
+    You then also need to add the import from @angular/forms  in the app.module.ts file:
+
+    import { FormsModule } from '@angular/forms';
+     */
 }
